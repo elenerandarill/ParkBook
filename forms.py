d56bedceb6ea069fd1edcd9efb9af0d22bedea9d
andarill <andarill@gmail.com>
2020-04-19 22:49:39 +0200

    Dziala rejestracja, sprawdzanie pól, przekierowanie do logowania i sprawdzenie dlugosci hasla.

--- forms.py
+++ forms.py
@@ -7,7 +7,7 @@ from models import User
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=30)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=3)])
     conf_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Create an Account')
 
