7d36ccb9d3e4b00677abbc0f94528d7a6ce914c0
andarill <andarill@gmail.com>
2020-04-19 22:35:35 +0200

    da sie rejestrowac i sprawdzane sa wszystkie pola do wprowadzania contentu. I po udanej rejestracji przekierowuje na strone Logowania!

--- console.py
+++ console.py
@@ -4,12 +4,17 @@ from PbApp import pb_db
 """creating first users for db"""
 # pb_db.create_all()
 #
-# user01 = User(username='user01', email='user01@wp.pl', password='user01')
+# user01 = User(username='aaa', email='aaa@aa.aa', password='aaa')
 # pb_db.session.add(user01)
-# user02 = User(username='user02', email='user02@wp.pl', password='user02')
+# user02 = User(username='bbb', email='bbb@bb.bb', password='bbb')
 # pb_db.session.add(user02)
 # pb_db.session.commit()
-#
+
+"""Deleted all users"""
+# for user in User.query.all():
+#     pb_db.session.delete(user)
+#     pb_db.session.commit()
+
 print(User.query.all())
 
 """creating parking spaces for db"""
